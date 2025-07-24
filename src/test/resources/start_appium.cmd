4849aac64da376c7a52c3e02cbd820302a5b3b3f
sharath_sd <sharathsd123@gmail.com>
2025-07-25 03:06:14 +0530

    changes

--- src/test/resources/start_appium.cmd
+++ src/test/resources/start_appium.cmd
@@ -1,8 +1,3 @@
 @echo off
-echo Starting Appium server...
-
-REM Start Appium in background (minimized, no input redirection issues)
+echo Launching Appium Server...
 start /min "" cmd /c "appium --base-path /wd/hub --port 4723 --log-level error"
-
-REM Give Appium server time to start
-ping 127.0.0.1 -n 10 >nul
