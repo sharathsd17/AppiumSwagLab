143cb7af1234d9df8dee10ee5527d26af4959174
sharath_sd <sharathsd123@gmail.com>
2025-07-25 03:20:10 +0530

    same

--- src/test/resources/start_appium.cmd
+++ src/test/resources/start_appium.cmd
@@ -1,4 +1,10 @@
 @echo off
 echo Launching Appium Server...
+
+REM Kill any old Appium instance on port 4723
+for /f "tokens=5" %%a in ('netstat -aon ^| find ":4723" ^| find "LISTENING"') do (
+    taskkill /F /PID %%a
+)
+
+REM Start Appium minimized in the background
 start "" /MIN cmd /c "appium --base-path /wd/hub --port 4723 --log-level error"
-timeout /t 10 >nul
