ca110750c250968fcc0cbf487e578a3302175665
sharath_sd <sharathsd123@gmail.com>
2025-07-25 03:17:38 +0530

    s

--- src/test/resources/start_appium.cmd
+++ src/test/resources/start_appium.cmd
@@ -1,4 +1,4 @@
 @echo off
 echo Launching Appium Server...
-start /b appium --base-path /wd/hub --port 4723 --log-level error
-timeout /t 2
+start "" /MIN cmd /c "appium --base-path /wd/hub --port 4723 --log-level error"
+timeout /t 10 >nul
